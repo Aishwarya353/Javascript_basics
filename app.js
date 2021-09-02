27975f7ceabf60d9d9509ead31917d320baecb72
Aishwarya353 <aishwaryav353@gmail.com>
2021-09-02 20:08:52 +0530

    datatypes

--- app.js
+++ app.js
@@ -1,5 +1,24 @@
-console.log("Hey this is my first piece of js");
-console.log(21548674 ,false,"fgghuv",{name:"aishu"});
-console.warn("This is a warning")
-console.error("this is an error");
-console.log('clear')
+// datatype
+// primitive and non primitive
+// Variables
+//var universal scope so go with let
+
+var name="Aishu"//"" is similar to ''
+name="Nisha"
+name=""
+let age=20;
+console.log(name)
+console.log(age)
+console.log(typeof age)
+console.log(typeof name)
+let brand
+console.log(brand)//undefined
+
+//Non primitive
+let arr=[1,2,3,4,5,6,7,8,9,10]
+arr.push("value")
+arr.push(true)
+arr.unshift("y")
+arr.unshift(".")
+console.log(typeof arr)
+console.log(arr)
