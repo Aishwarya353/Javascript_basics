9295367e8306793bc8ebc0cbd2b108b7d473f808
Aishwarya353 <aishwaryav353@gmail.com>
2021-09-02 19:29:10 +0530

    Console Basics

---
+++ app.js
@@ -0,0 +1,5 @@
+console.log("Hey this is my first piece of js");
+console.log(21548674 ,false,"fgghuv",{name:"aishu"});
+console.warn("This is a warning")
+console.error("this is an error");
+console.log('clear')
