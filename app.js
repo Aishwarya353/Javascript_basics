d181df8ddea3cbf379455207f535e070bc2bb40e
Aishwarya353 <aishwaryav353@gmail.com>
2021-09-03 20:48:21 +0530

    Conditional Statements

--- app.js
+++ app.js
@@ -1,24 +1,17 @@
-//Logical Operators
-const a = 2
-const b = 3
-console.log('a == b', a == b)
-console.log('a != b', a != b)
-console.log('')
-
-console.log('Truth table for AND')
-console.log('')
-
-console.log('false && false', false && false)
-console.log('true && true', true && true)
-console.log('true && false', true && false)
-console.log('false && true', false && true)
-console.log('')
-
-console.log('Truth table for OR')
-console.log('')
-
-console.log('false|| false', false || false)
-console.log('true|| true', true || true)
-console.log('true|| false', true || false)
-console.log('false|| true', false || true)
-
+//Conditional statements
+const a=0
+if(typeof a==='number' && a!=0){
+    if(a%2===0)//use 3 =
+    {
+        console.log("even")
+    }
+    else{
+        console.log("odd")
+    }
+}
+else if(a===0){
+    console.log("Neither even nor odd")
+}
+else{
+    console.log("Not a number")
+}
