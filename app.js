3c9c4ad08cb5a134271478ae0724950c1f077f76
Aishwarya353 <aishwaryav353@gmail.com>
2021-09-03 20:04:37 +0530

    Comparison operators

--- app.js
+++ app.js
@@ -1,24 +1,15 @@
-let Name="Aishwarya"
-let age=20
-console.log(Name.bold())
-console.log(age.toString())
-console.log(typeof age)
-//operators
-const a=2;
-const b=15;
-let c=0;
-console.log(a + b);
-console.log(a - b);
-console.log(a * b);
-console.log(a / b);
-console.log(a ** b);
-console.log(b % a);
-console.log(age + b);
-console.log("---------------------------------------------------------------------------------");
-c+=1
-c++//c+=1
-console.log(c);
-c*=5
-console.log(c);//c=c*5;
-c**=3
-console.log(c);
+//comparison operators
+const a=21
+const b=7
+const c=21
+console.log(a>b)
+console.log(a<b)
+console.log(a==b)
+console.log(a==c)
+console.log(a>=b)
+const e=11
+const f="11"
+console.log(a==b)
+console.log(a>b)
+console.log(a===b)//check type also (ie e==f && number==string=t && f=f)
+console.log("a"=="A")
